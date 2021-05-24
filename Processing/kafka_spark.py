81c7229e903f3cdd39104607d78bf3de61811f06
Sanika Kulkarni <sanika.k.goa@gmail.com>
2021-05-25 00:24:56 +0530

    structured streaming

--- Processing/kafka_spark.py
+++ Processing/kafka_spark.py
@@ -1,5 +1,5 @@
 import findspark
-findspark.init('/opt/Spark')
+findspark.init('/opt/spark')
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -10,16 +10,16 @@ if __name__=="__main__":
     sc = SparkContext(appName="Kafka Spark Demo")
     sc.setLogLevel("WARN")
     ssc = StreamingContext(sc,5)
-    message = KafkaUtils.createDirectStream(ssc, topics=['twitter'],kafkaParams= {"metadata.broker.list":"localhost:9092"})
+    # message = KafkaUtils.createDirectStream(ssc, topics=['twitter'],kafkaParams= {"metadata.broker.list":"localhost:9092"})
 
-    words = message.map(lambda x: x[1]).flatMap(lambda x: x.split(" "))
+    # words = message.map(lambda x: x[1]).flatMap(lambda x: x.split(" "))
 
-    wordcount = words.map(lambda x: (x,1)).reduceByKey(lambda a,b: a+b)
+    # wordcount = words.map(lambda x: (x,1)).reduceByKey(lambda a,b: a+b)
 
-    wordcount.pprint()
+    # wordcount.pprint()
 
-    ssc.start()
-    ssc.awaitTermination()
-    sc.stop()
+    # ssc.start()
+    # ssc.awaitTermination()
+    # sc.stop()
 
     
